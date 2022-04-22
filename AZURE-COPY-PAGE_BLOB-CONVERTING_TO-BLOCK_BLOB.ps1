bfdc336cd437ef343a47f40ccefc3f60a1647eab
GuiMunizR <guilherme.muniz@outlook.com.br>
2022-04-22 02:23:32 -0300

    Commit 20220422-0223

--- AZURE-COPY-PAGE_BLOB-CONVERTING_TO-BLOCK_BLOB.ps1
+++ AZURE-COPY-PAGE_BLOB-CONVERTING_TO-BLOCK_BLOB.ps1
@@ -1,4 +1,3 @@
-#Created by: Warley Roverse (linkedin.com/in/warleyroverse)
-##Edited and revised by: Guilherme Muniz (linkedin.com/in/guilhermemunizr)
-###NAME: MULTIUSER-CONVERT_MAILBOX_IN_SHAREDMAILBOX-AND-REMOVE_LICENSES
-####OBJETIVE: 
+#Created by: Guilherme Muniz (linkedin.com/in/guilhermemunizr)
+##NAME: AZURE-COPY-PAGE_BLOB-CONVERTING_TO-BLOCK_BLOB
+###OBJETIVE: 
